48efa62a63db9d9bcadd46fb06b7eab897bb604c
Kassio Coutinho <kassio.coutinho@mantech.com>
2024-05-21 14:59:11 -0500

    initial exercises
    python script
    helm chart
    docker image

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# merger
-Merge stuff
+# nightcrawler
+Welcome to the Interview Questions Project repository! This project aims to be a comprehensive resource for technical interviews.
+
+## Requirements
+* kubernetes cluster (k3d,minikube,etc..)
+* container runtime (docker,containerd,podman)
+* helm
+* git
+* python
+
+## Python exercise
+
+**API KEY**: 3b93dbe006af066f0a0db26c13d3a15c
+
+**API DOCS**:
+  https://openweathermap.org/api/geocoding-api
+  https://openweathermap.org/current
+
+1. Resolve issues with script
+2. Add parse arg for unit of measurement, look at api docs for units
+3. Update get_daily_weather function to return imperial measurement instead of Kelvin using units argument created above
+4. Create a new function that returns a string with the following data: temp, temp_min, temp_max, wind speed, and weather description
+5. Print out data above
+
+## Docker execise
+
+1. Create test file with "Hello Worlds!" in the Dockerfile within the `/nginx/files` dir
+2. Build Dockerfile and tag image
+3. Run docker image targetport 8080
+4. Access http server on localhost
+
+## Helm Chart exercise
+
+1. Resolve issue with helm chart
+2. Add service manifest to helm chart that will expose nginx deployment
+3. Deploy helm chart and curl service
+
+## K8s Imperative exercises
---
+++ python/weather.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import argparse
+import os
+import sys
+from typing import Tuple
+
+import requests
+
+
+API_KEY = os.getenv('OW_API')
+
+class Weather:
+
+  def __init__(self, args) -> None:
+      self.args = args
+      self.api_key = API_KEY
+      self.session = self._session()
+
+  def _session(self) -> requests.Session:
+    session = requests.Session()
+    session.verify = False
+    return session
+
+  def _get(self, url: str) -> requests.Response:
+        response = self.session.get(url)
+        response.raise_for_status()
+        return response
+
+  def _get_coord(self) -> Tuple[str,str]:
+      latitude = ""
+      longitude = ""
+      coordinates_url= f"http://api.openweathermap.org/geo/1.0/zip?zip={self.args.zip},{self.args.country}&appid={self.api_key}"
+
+      response = self._get(coordinates_url)
+      coordinates = response.json()
+
+      latitude = coordinates['ltu']
+      longitude = coordinates['lgn']
+
+      return latitude, longitude
+
+  def get_daily_weather(self, latitude: str, longitude: str):
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={self.api_key}"
+    self.get(weather_url)
+
+if __name__ == "__main__":
+
+  if not API_KEY:
+    print("API key does not exist for open weather")
+    sys.exit(1)
+
+  parser = argparse.ArgumentParser(description= 'Generates current weather data')
+  parser.add_argument('--zip', type=str, help="enter zipcode for weather data", required=True)
+  parser.add_argument('--country', '-c', type=str, default='US', help="enter country abbreviation")
+  args = parser.parse_args()
+
+  weather = Weather(args)
+  latitude, longitude = weather._get_coord()
+  weather.get_daily_weather(latitude, longitude)
